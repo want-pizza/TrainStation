0100788ea62a0f3c77f57cc3e1257f33a778eb3a
max-is-one <maksal2005@gmail.com>
2022-07-18 15:18:01 +0300

    Size

--- User_Form.Designer.cs
+++ User_Form.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // User_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(802, 672);
+            this.Name = "User_Form";
             this.Text = "User_Form";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
